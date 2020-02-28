35e8bd7b46f0ace62aaa79bb5fa794ba6c454dd3
cjogles <jackson.ogles28@gmail.com>
2020-02-28 11:23:54 -0700

    added styling to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,30 +1,47 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Footer() {
   return (
     <>
-      <section className='sectionfooter'>
-      <div className='footerSection1'>
-          <div className='col1'>
-            <h2>Lindys Local</h2>
-            <p>Locations</p>
-            <p>Customer Portal</p>
-            <p>Privacy Policy</p>
+      <section className="sectionfooter">
+        <div className="footerSection1">
+          <div className="col1">
+            <Link to="/">
+              <h2>Lindys Local</h2>
+            </Link>
+            <Link to="/locations">
+              <p>Locations</p>
+            </Link>
+            <a
+              href="https://salesrabbit.com"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              <p className="navLinks">Customer Portal</p>
+            </a>
+            <Link to="/privacy">
+              <p>Privacy Policy</p>
+            </Link>
           </div>
-          <div className='col2'>
+          <div className="col2">
             <h2>Mission Statement</h2>
+            <p>
+              We provide a local touch to pest control service by taking care of
+              pests so you don't have to.
+            </p>
           </div>
-          <div className='col3'>
+          <div className="col3">
             <h2>Hours</h2>
-            <p>Monday - Friday</p>
+            <p className='bold'>Monday - Friday</p>
             <p>7am - 7pm MST</p>
-            <p>Saturday</p>
+            <p className='bold'>Saturday</p>
             <p>7am - 4pm MST</p>
-            <p>Sunday</p>
+            <p className='bold'>Sunday</p>
             <p>Closed</p>
           </div>
         </div>
-        <div className='footerSection2'>
+        <div className="footerSection2">
           <p>© Copyright 2020 Lindy's Local. ALL RIGHTS RESERVED.</p>
           <a target="_blank" href="https://icons8.com">
             Icons from Icons8
