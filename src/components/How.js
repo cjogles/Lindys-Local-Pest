3c90c7f92c223c47bdc8c6e929f2de077beb986e
cjogles <jackson.ogles28@gmail.com>
2020-03-26 13:47:17 -0600

    finished last second changes

--- src/components/How.js
+++ src/components/How.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
-import Footer from '../components/Footer';
 
 export default function Bugs() {
 
@@ -12,7 +10,7 @@ export default function Bugs() {
         </div>
         <div className='howChart'>
           <div className='explainHow'>
-            <p>By using professionally trained applicators whose whole purpose is to Observe your home, Identify all pests, Evaluate the best plan of action, and Execute , this combined with using only the safest, eco-friendly, and highest valued products on the market guarantees a satisfied customer, everytime!</p>
+            <p>By using professionally trained applicators whose whole purpose is to observe your home, identify all pests, evaluate the best plan of action, and then execute it, this combined with using only the safest, eco-friendly, and highest valued products on the market, guarantees a satisfied customer, everytime!</p>
           </div>
          
         </div>
